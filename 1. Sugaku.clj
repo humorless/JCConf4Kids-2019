4742de687a5cbcd264e32f146dbbd641c13b9df8
skalster <skalster@users.noreply.github.com>
2019-06-03 04:27:56 +0200

    Add files via upload
    First draft of basic math lesson.

---
+++ 1. Sugaku.clj
@@ -0,0 +1,76 @@
+(comment "
+http://www.kita9.ed.jp/eductr/Handbook/Challengesheet/Elementaryschool/sansuu/3/301.pdf
+Basic mathematics:
+きほんの算数（さんすう）
+Humans needs to learn calculations. There are patterns:
+計算（けいさん）を勉強（べんきょう）していくと、いろいろな公式（こうしき）をおぼえます。
+
+Computers are good for large calculations for example several millions numbers or reptitive
+caculations, for example calculate 5 + 5 a thousand times
+コンピューターは大きな計算（けいさん）が得意（とくい）です。たとえば、数百万（すうひゃくまん）たんいの計算や、なんどもくり返す計算、５＋５を千回くり返すこともできます。
+
+Basic calculations きほんの計算（けいさん） 
+")
+
+
+
+;http://www.kita9.ed.jp/eductr/Handbook/Challengesheet/Elementaryschool/sansuu/3/304.pdf 
+:Addition たし算　：　５＋６
+(+ 5 6)
+
+(comment "
+In school or on paper we write like this 5 + 6, computers understand different.
+
+ふつう、学校では５＋６と書きますが、コンピューターがわかるためにちがう方法（ほうほう）で書きます。
+( means start, + means addition, and ) means stop
+　は、式のはじまり
+
+＋　は、たし算
+
+　は、式のおわり、という意味（いみ）です。
+")
+
+;Multiplication かけ算　：　２×４
+; http://www.kita9.ed.jp/eductr/Handbook/Challengesheet/Elementaryschool/sansuu/3/301.pdf
+(* 2 4)
+
+(comment "
+( means start, * means multiplication, and ) means stop
+（　は、式のはじまり
+
+*　は、かけ算
+
+）　は、式のおわり、という意味（いみ）です。")
+
+ 
+
+;Substraction ひき算　：　８−４
+; http://www.kita9.ed.jp/eductr/Handbook/Challengesheet/Elementaryschool/sansuu/3/302.pdf
+(- 8 4)
+
+(comment "
+( means start, - means substraction, and ) means stop
+（　は、式のはじまり
+
+-　は、ひき算
+
+）　は、式のおわり、という意味（いみ）です。")
+
+
+;Division わり算　：　１０÷５
+; http://www.kita9.ed.jp/eductr/Handbook/Challengesheet/Elementaryschool/sansuu/3/308.pdf
+(/ 10 5)
+
+(comment "
+( means start, / means division, and ) means stop
+
+（　は、式のはじまり
+
+/　は、わり算
+
+）　は、式のおわり、という意味（いみ）です。 ")
+
+(mod 5 2) ;It means divide by two and show the remainder, in this case 2 * 2 = 4, 5-4= 1 (remainder)
+
+;Fractions - Addition and substruction - 
+;http://www.kita9.ed.jp/eductr/Handbook/Challengesheet/Elementaryschool/sansuu/3/313.pdf
